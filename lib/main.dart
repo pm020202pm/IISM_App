12e348c4d26b0b83982100f67d8ea755621ceeac
pm020202pm <pm020202pm@gmail.com>
2024-12-07 11:44:27 +0000

    modified teams page

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,6 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: SplashScreen(),
       home: const DashBoard(index: 0),
     );
   }
